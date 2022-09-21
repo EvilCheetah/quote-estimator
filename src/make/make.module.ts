5ba4ea4e615f29f0eec2743ab2857ca442c9d582
Eugene Moshchyn <eugene.moschyn@gmail.com>
2022-09-21 10:48:41 -0700

    format(): indentation

--- src/make/make.module.ts
+++ src/make/make.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { MakeService } from './make.service';
 import { MakeController } from './make.controller';
 
+
 @Module({
-  controllers: [MakeController],
-  providers: [MakeService]
+    controllers: [MakeController],
+    providers:   [MakeService]
 })
 export class MakeModule {}
