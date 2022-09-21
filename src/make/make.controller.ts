1de43104ce03b28ccc5b67f82a3313de7bc9658c
Eugene Moshchyn <eugene.moschyn@gmail.com>
2022-09-21 14:11:53 -0700

    update(): return and params types

--- src/make/make.controller.ts
+++ src/make/make.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Make } from '@prisma/client';
+import { Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe } from '@nestjs/common';
 import { MakeService } from './make.service';
 import { CreateMakeDTO } from './dto/create-make.dto';
 import { UpdateMakeDTO } from './dto/update-make.dto';
@@ -9,33 +10,53 @@ export class MakeController
 {
     constructor(private readonly makeService: MakeService) {}
 
+
     @Post()
-    async create(@Body() createMakeDTO: CreateMakeDTO)
+    async create(
+        @Body()
+        createMakeDTO: CreateMakeDTO
+    ): Promise<Make>
     {
         return this.makeService.create(createMakeDTO);
     }
 
+
     @Get()
-    async find_all()
+    async find_all(): Promise<Make[]>
     {
         return this.makeService.find_all();
     }
 
+
     @Get(':make_id')
-    async find_one(@Param('make_id') make_id: string)
+    async find_one(
+        @Param('make_id', ParseIntPipe)
+        make_id: number
+    ): Promise<Make>
     {
-        return this.makeService.find_one(+make_id);
+        return this.makeService.find_one(make_id);
     }
+
 
     @Patch(':make_id')
-    async update(@Param('make_id') make_id: string, @Body() updateMakeDTO: UpdateMakeDTO)
+    async update(
+        @Param('make_id', ParseIntPipe)
+        make_id: number,
+        
+        @Body()
+        updateMakeDTO: UpdateMakeDTO
+    ): Promise<Make>
     {
-        return this.makeService.update(+make_id, updateMakeDTO);
+        return this.makeService.update(make_id, updateMakeDTO);
     }
 
+    
     @Delete(':make_id')
-    async remove(@Param('make_id') make_id: string)
+    async remove(
+        @Param('make_id', ParseIntPipe)
+        make_id: number
+    ): Promise<Make>
     {
-        return this.makeService.remove(+make_id);
+        return this.makeService.remove(make_id);
     }
 }
