76a641fa026ac955853452393e3238e87ef6b66b
Eugene Moshchyn <eugene.moschyn@gmail.com>
2022-09-21 13:49:59 -0700

    update(): prisma import

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MakeModule } from './make/make.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { PrismaService } from './prisma/prisma.service';
 
 
 @Module({
-    imports:     [MakeModule],
-    controllers: [],
-    providers:   [],
+    imports: [
+        PrismaModule,
+        MakeModule
+    ]
 })
 export class AppModule {}
