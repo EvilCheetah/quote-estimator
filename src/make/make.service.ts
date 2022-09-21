cd864d52eea5e9e660751a952bf7dceb931090ca
Eugene Moshchyn <eugene.moschyn@gmail.com>
2022-09-21 14:12:10 -0700

    update(): return types

--- src/make/make.service.ts
+++ src/make/make.service.ts
@@ -1,3 +1,4 @@
+import { Make } from '@prisma/client';
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 
@@ -12,7 +13,7 @@ export class MakeService
         private readonly prisma: PrismaService
     ) {}
 
-    async create(createMakeDTO: CreateMakeDTO)
+    async create(createMakeDTO: CreateMakeDTO): Promise<Make>
     {
         const {make_name, year} = createMakeDTO;
 
@@ -21,39 +22,39 @@ export class MakeService
         })
     }
 
-    async find_all()
+    async find_all(): Promise<Make[]>
     {
         return this.prisma.make.findMany();
     }
 
-    async find_one(make_id: number)
+    async find_one(make_id: number): Promise<Make>
     {
         const make = await this.prisma.make.findUnique({
             where: { make_id }
         });
 
         if ( !make )
-            throw new NotFoundException(`Make with id: ${make_id} was NOT FOUND`);
+            throw new NotFoundException(`Make with id: '${make_id}' was NOT FOUND`);
         
         return make;
     }
 
-    async update(make_id: number, updateMakeDTO: UpdateMakeDTO)
+    async update(make_id: number, updateMakeDTO: UpdateMakeDTO): Promise<Make>
     {
         const make = await this.find_one(make_id);
 
         return this.prisma.make.update({
-            where: { make_id },
+            where: { make_id: make.make_id },
             data:  updateMakeDTO
         });
     }
 
-    async remove(make_id: number)
+    async remove(make_id: number): Promise<Make>
     {
         const make = await this.find_one(make_id);
 
         return this.prisma.make.delete({
-            where: { make_id }
+            where: { make_id: make.make_id }
         });
     }
 }
