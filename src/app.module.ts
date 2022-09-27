d5276fde041c701c3c15ae6749c73f0cc1e6d9e8
Eugene M <eugene.moschyn@gmail.com>
2022-09-27 16:24:29 -0700

    remove(): unused imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,5 @@
 import { Module } from '@nestjs/common';
-import { RouterModule } from '@nestjs/core';
 import { PrismaModule } from './prisma/prisma.module';
-import { MakeModule } from './vehicle/make/make.module';
-import { ModelModule } from './vehicle/model/model.module';
 import { VehicleModule } from './vehicle/vehicle.module';
 
 
@@ -10,8 +7,6 @@ import { VehicleModule } from './vehicle/vehicle.module';
     imports: [
         PrismaModule,
         VehicleModule
-        // MakeModule,
-        // ModelModule
     ]
 })
 export class AppModule {}
