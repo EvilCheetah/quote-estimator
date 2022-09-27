3de42bafb306106353087acf2c1aeb39cfcaeec7
Eugene M <eugene.moschyn@gmail.com>
2022-09-27 16:24:07 -0700

    update(): imports and fix imports - remove unused fix - decorator typo

--- src/vehicle/vehicle.module.ts
+++ src/vehicle/vehicle.module.ts
@@ -1,14 +1,10 @@
 import { Module } from "@nestjs/common";
-import { RouterModule } from "@nestjs/core";
-import { MakeController } from "./make/make.controller";
+
 import { MakeModule } from "./make/make.module";
-import { MakeService } from "./make/make.service";
-import { ModelController } from "./model/model.controller";
 import { ModelModule } from "./model/model.module";
-import { ModelService } from "./model/model.service";
 
 
-Module({
+@Module({
     imports: [
         MakeModule,
         ModelModule
