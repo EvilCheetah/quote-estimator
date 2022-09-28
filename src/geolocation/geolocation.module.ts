188e790a36cd08f4f6f3464304afe2b3e997a08c
Eugene Moshchyn <eugene.moschyn@gmail.com>
2022-09-28 10:58:58 -0700

    add(): module for geo-related modules

---
+++ src/geolocation/geolocation.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { CityModule } from './city/city.module';
+
+
+@Module({
+    imports: [
+        CityModule
+    ]
+})
+export class GeolocationModule {}
