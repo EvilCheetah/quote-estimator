bdddca93aa767bda1ec611def2adaaf217dc39ec
Eugene Moshchyn <eugene.moschyn@gmail.com>
2022-09-28 13:45:00 -0700

    add(): validation to state

--- src/geolocation/state/dto/create-state.dto.ts
+++ src/geolocation/state/dto/create-state.dto.ts
@@ -1,1 +1,13 @@
-export class CreateStateDto {}
+import { IsString, MaxLength, MinLength } from "class-validator";
+
+
+export class CreateStateDto
+{
+    @IsString()
+    state_name:         string;
+    
+    @IsString()
+    @MinLength(2)
+    @MaxLength(2)
+    state_abbreviation: string;
+}
