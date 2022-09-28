d9f7bea0238121730c306bcac86be7e7d1c55d8b
Eugene Moshchyn <eugene.moschyn@gmail.com>
2022-09-28 14:10:32 -0700

    add(): validation

--- src/geolocation/city/dto/create-city.dto.ts
+++ src/geolocation/city/dto/create-city.dto.ts
@@ -1,1 +1,11 @@
-export class CreateCityDTO {}
+import { IsInt, IsString } from "class-validator";
+
+
+export class CreateCityDTO
+{
+    @IsInt()
+    state_id:  number;
+
+    @IsString()
+    city_name: string;
+}
--- src/geolocation/city/dto/update-city.dto.ts
+++ src/geolocation/city/dto/update-city.dto.ts
@@ -1,4 +1,5 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateCityDTO } from './create-city.dto';
 
+
 export class UpdateCityDTO extends PartialType(CreateCityDTO) {}
