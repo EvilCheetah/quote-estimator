81a099180b071106bf314157d07319c061f74f86
Eugene Moshchyn <eugene.moschyn@gmail.com>
2022-09-28 11:14:58 -0700

    add(): import state module

--- src/geolocation/geolocation.module.ts
+++ src/geolocation/geolocation.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CityModule } from './city/city.module';
+import { StateModule } from './state/state.module';
 
 
 @Module({
     imports: [
+        StateModule,
         CityModule
     ]
 })
