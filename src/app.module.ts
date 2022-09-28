94d003f768619333176ebdfdd43c331b103f8af6
Eugene Moshchyn <eugene.moschyn@gmail.com>
2022-09-28 10:59:44 -0700

    add(): geolocation module import

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { VehicleModule } from './vehicle/vehicle.module';
+import { GeolocationModule } from './geolocation/geolocation.module';
 
 
 @Module({
     imports: [
         PrismaModule,
-        VehicleModule
+        VehicleModule,
+        GeolocationModule,
     ]
 })
 export class AppModule {}
