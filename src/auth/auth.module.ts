5700252680a67b87f2bbfad534ba67e076411593
Eugene Moshchyn <eugene.moschyn@gmail.com>
2022-10-24 14:10:23 -0700

    format(): indentation

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 
+
 @Module({
-  controllers: [AuthController],
-  providers: [AuthService]
+    controllers: [AuthController],
+    providers:   [AuthService]
 })
 export class AuthModule {}
