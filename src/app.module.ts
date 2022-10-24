796a4b024e8cab63da46afdf2133862ae04c1efe
Eugene Moshchyn <eugene.moschyn@gmail.com>
2022-10-24 13:31:54 -0700

    add(): config module to import

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,14 @@ import { PrismaModule } from './prisma/prisma.module';
 import { VehicleModule } from './vehicle/vehicle.module';
 import { GeolocationModule } from './geolocation/geolocation.module';
 import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
 
 
 @Module({
     imports: [
+        ConfigModule.forRoot({ isGlobal: true }),
         PrismaModule,
+
         VehicleModule,
         GeolocationModule,
         UsersModule,
