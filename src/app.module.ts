f1e77a995b4904994d74bfe0f887033c6cc1a47a
Eugene Moshchyn <eugene.moschyn@gmail.com>
2022-10-24 13:43:20 -0700

    update(): imports section

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,26 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { VehicleModule } from './vehicle/vehicle.module';
 import { GeolocationModule } from './geolocation/geolocation.module';
-import { UsersModule } from './users/users.module';
-import { ConfigModule } from '@nestjs/config';
 
 
 @Module({
     imports: [
+        /// Library Imports
         ConfigModule.forRoot({ isGlobal: true }),
         PrismaModule,
 
+        /// Auth Imports
+        UsersModule,
+        AuthModule,
+
+        /// Services Imports
         VehicleModule,
         GeolocationModule,
-        UsersModule,
     ]
 })
 export class AppModule {}
