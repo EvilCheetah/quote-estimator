945fcf44ee8ac3ee36edf45e70f5dc886e6e6b7c
Eugene Moshchyn <eugene.moschyn@gmail.com>
2022-10-28 10:43:52 -0700

    add(): basic structure for super user generation

---
+++ prisma/create-super-user.ts
@@ -0,0 +1,37 @@
+import { PrismaClient } from "@prisma/client";
+
+
+async function create_super_user(prisma: PrismaClient)
+{
+    ///--------------- Create SuperUser --------------///
+    await prisma.user.create({
+        data: {
+            username: process.env.SUPER_USER_USERNAME,
+            email:    process.env.SUPER_USER_EMAIL,
+            password: process.env.SUPER_USER_PASSWORD
+        }
+    });
+}
+
+
+async function main() {
+    const prisma = new PrismaClient();
+
+    try
+    {
+        await create_super_user(prisma)
+    }
+    
+    catch (exception)
+    {
+        console.error(exception)
+    }
+    
+    finally
+    {
+        await prisma.$disconnect()
+    }
+}
+
+
+main()
