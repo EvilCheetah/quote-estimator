62c6c915085334f20e586fe10e81b87f589e772d
Eugene Moshchyn <eugene.moschyn@gmail.com>
2022-10-31 14:27:48 -0700

    add(): two decorators

--- src/common/decorator/index.ts
+++ src/common/decorator/index.ts
@@ -1,3 +1,5 @@
 export * from './get-user.decorator';
+export * from './is-equal-to.decorator';
 export * from './is-username.decorator';
 export * from './is-non-negative.decorator';
+export * from './is-strong-password.decorator';
