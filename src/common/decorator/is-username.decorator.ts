2d782859b035412b06fe889f1a319de1cf441858
Eugene Moshchyn <eugene.moschyn@gmail.com>
2022-10-31 10:13:53 -0700

    add(): dto decorator for username

---
+++ src/common/decorator/is-username.decorator.ts
@@ -0,0 +1,29 @@
+import { buildMessage, isString, matches, ValidateBy, ValidationOptions } from "class-validator";
+
+import { USERNAME_PATTERN } from "@common/constant";
+
+
+export const IS_USERNAME = 'isUsername';
+
+
+export function isUsername(value: unknown): boolean
+{
+    return isString(value) && matches(value, USERNAME_PATTERN)
+}
+
+
+export function IsUsername(validationOptions?: ValidationOptions)
+{
+    return ValidateBy({
+        name: IS_USERNAME,
+        validator: {
+            validate: (value, args) => isUsername(value),
+            defaultMessage: buildMessage(
+                (each_prefix) => (
+                    each_prefix + '$property must be a proper username'
+                ),
+                validationOptions
+            )
+        }
+    })
+}
