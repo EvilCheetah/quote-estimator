eefd3a88106378cc08666a6aab932901ba5a6aff
Eugene Moshchyn <eugene.moschyn@gmail.com>
2022-11-01 10:18:17 -0700

    update(): hard-coded with const values

--- src/common/dto/new-user.dto.ts
+++ src/common/dto/new-user.dto.ts
@@ -1,6 +1,7 @@
 import { IsEmail, IsString, MaxLength } from "class-validator";
 
 import { IsEqualTo, IsStrongPassword, IsUsername } from "@common/decorator";
+import { MAX_ALLOWED_PASSWORD_LENGTH, MAX_ALLOWED_USERNAME_LENGTH } from "@common/constant";
 
 
 export class NewUserDTO
@@ -9,15 +10,15 @@ export class NewUserDTO
     email:            string;
 
     @IsUsername()
-    @MaxLength(120)
+    @MaxLength( MAX_ALLOWED_USERNAME_LENGTH )
     username:         string;
 
     @IsStrongPassword()
-    @MaxLength(120)
+    @MaxLength( MAX_ALLOWED_PASSWORD_LENGTH )
     password:         string;
 
     @IsString()
-    @MaxLength(120)
+    @MaxLength( MAX_ALLOWED_PASSWORD_LENGTH )
     @IsEqualTo('password', { 'message': "'confirm_password' should match 'password''" })
     confirm_password: string;
 }
