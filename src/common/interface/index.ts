87f92b7d8efa4fb08114daa2a4abe6be3cd0b247
Eugene Moshchyn <eugene.moschyn@gmail.com>
2022-11-01 14:07:27 -0700

    add(): jwt payload to specify jwt data in payload

--- src/common/interface/index.ts
+++ src/common/interface/index.ts
@@ -1,5 +1,6 @@
 export * from './dimensions.interface';
 
-export * from './jwt-tokens.interface'
+export * from './jwt-tokens.interface';
+export * from './jwt-payload.interface';
 export * from './access-token.interface';
 export * from './refresh-token.interface';
