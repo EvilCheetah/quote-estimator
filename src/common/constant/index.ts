47370e9b803c686f5d62469cbe43c433d6dee188
Eugene Moshchyn <eugene.moschyn@gmail.com>
2022-11-01 10:16:36 -0700

    create(): const for string length

--- src/common/constant/index.ts
+++ src/common/constant/index.ts
@@ -2,3 +2,6 @@ export * from './model-options.query';
 
 export * from './pattern/username.pattern';
 export * from './pattern/strong-password.pattern';
+
+export * from './max-username-length.constant';
+export * from './max-password-length.constant';
---
+++ src/common/constant/max-username-length.constant.ts
@@ -0,0 +1,1 @@
+export const MAX_ALLOWED_USERNAME_LENGTH = 120;
