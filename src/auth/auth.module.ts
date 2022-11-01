7082267b854180d586b289e4acf727e1b44861f2
Eugene Moshchyn <eugene.moschyn@gmail.com>
2022-11-01 14:09:18 -0700

    add(): jwt module import

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { PassportModule } from '@nestjs/passport';
+
 import { AuthService } from './auth.service';
+import { UsersModule } from '../users/users.module';
 import { AuthController } from './auth.controller';
 
 
 @Module({
+    imports: [
+        UsersModule,
+        PassportModule,
+
+        JwtModule.registerAsync({
+            useFactory: (configService: ConfigService) => ({
+                secret: configService.get<string>('JWT_SECRET_KEY'),
+                signOptions: { 
+                    expiresIn: configService.get<string>('JWT_EXPIRES_IN')
+                }
+            }),
+            inject: [ConfigService]
+        })
+    ],
     controllers: [AuthController],
     providers:   [AuthService]
 })
