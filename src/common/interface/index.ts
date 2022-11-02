68a36b15d5de10b1070b77376d7443a2196363f8
Eugene Moshchyn <eugene.moschyn@gmail.com>
2022-11-02 12:16:28 -0700

    add(): criteria interface to export

--- src/common/interface/index.ts
+++ src/common/interface/index.ts
@@ -4,3 +4,4 @@ export * from './jwt-tokens.interface';
 export * from './jwt-payload.interface';
 export * from './access-token.interface';
 export * from './refresh-token.interface';
+export * from './unique-user-criteria.interface';
