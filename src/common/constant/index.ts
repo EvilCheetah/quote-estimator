fc82c73e83642762b9e45bffd22338ae2fb30f6c
Eugene Moshchyn <eugene.moschyn@gmail.com>
2022-11-03 14:49:47 -0700

    update(): imports

--- src/common/constant/index.ts
+++ src/common/constant/index.ts
@@ -3,5 +3,5 @@ export * from './model-options.query';
 export * from './pattern/username.pattern';
 export * from './pattern/strong-password.pattern';
 
-export * from './max-username-length.constant';
-export * from './max-password-length.constant';
+export * from './limit/password.length';
+export * from './limit/username.length';
