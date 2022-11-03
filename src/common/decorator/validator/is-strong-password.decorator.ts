a2819f852d18884e72b8cfc2e68f19daf28945bf
Eugene Moshchyn <eugene.moschyn@gmail.com>
2022-11-03 14:25:55 -0700

    refactor(): used similar implementation as in class validator

--- src/common/decorator/validator/is-strong-password.decorator.ts
+++ src/common/decorator/validator/is-strong-password.decorator.ts
@@ -1,5 +1,5 @@
+import { buildMessage, matches, ValidateBy, ValidationOptions } from "class-validator";
 import { STRONG_PASSWORD } from "@common/constant";
-import { buildMessage, isString, matches, ValidateBy, ValidationOptions } from "class-validator";
 
 
 export const IS_STRONG_PASSWORD = 'isStrongPassword';
@@ -7,22 +7,25 @@ export const IS_STRONG_PASSWORD = 'isStrongPassword';
 
 export function isStrongPassword(value: unknown): boolean
 {
-    return isString(value) && matches(value, STRONG_PASSWORD);
+    return (typeof(value) === 'string') && matches(value, STRONG_PASSWORD);
 }
 
 
-export function IsStrongPassword(validationOptions?: ValidationOptions)
+export function IsStrongPassword(validationOptions?: ValidationOptions): PropertyDecorator
 {
-    return ValidateBy({
-        name: IS_STRONG_PASSWORD,
-        validator: {
-            validate: (value, args) => isStrongPassword(value),
-            defaultMessage: buildMessage(
-                (each_prefix) => (
-                    each_prefix + '$property must be a strong password'
-                ),
-                validationOptions
-            )
-        }
-    });
+    return ValidateBy(
+        {
+            name: IS_STRONG_PASSWORD,
+            validator: {
+                validate: (value, args) => isStrongPassword(value),
+                defaultMessage: buildMessage(
+                    (each_prefix) => (
+                        each_prefix + '$property must be a strong password'
+                    ),
+                    validationOptions
+                )
+            },
+        },
+        validationOptions
+    );
 }
--- src/common/decorator/validator/is-username.decorator.ts
+++ src/common/decorator/validator/is-username.decorator.ts
@@ -1,4 +1,4 @@
-import { buildMessage, isString, matches, ValidateBy, ValidationOptions } from "class-validator";
+import { buildMessage, matches, ValidateBy, ValidationOptions } from "class-validator";
 import { USERNAME_PATTERN } from "@common/constant";
 
 
@@ -7,22 +7,25 @@ export const IS_USERNAME = 'isUsername';
 
 export function isUsername(value: unknown): boolean
 {
-    return isString(value) && matches(value, USERNAME_PATTERN)
+    return (typeof(value) === 'string') && matches(value, USERNAME_PATTERN);
 }
 
 
-export function IsUsername(validationOptions?: ValidationOptions)
+export function IsUsername(validationOptions?: ValidationOptions): PropertyDecorator
 {
-    return ValidateBy({
-        name: IS_USERNAME,
-        validator: {
-            validate: (value, args) => isUsername(value),
-            defaultMessage: buildMessage(
-                (each_prefix) => (
-                    each_prefix + '$property must be a proper username'
-                ),
-                validationOptions
-            )
-        }
-    });
+    return ValidateBy(
+        {
+            name: IS_USERNAME,
+            validator: {
+                validate: (value, args) => isUsername(value),
+                defaultMessage: buildMessage(
+                    (each_prefix) => (
+                        each_prefix + '$property must be a proper username'
+                    ),
+                    validationOptions
+                )
+            }
+        }, 
+        validationOptions
+    );
 }
