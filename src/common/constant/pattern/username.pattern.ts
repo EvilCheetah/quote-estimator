b8067888b470f4659402e695923faf480d169248
Eugene Moshchyn <eugene.moschyn@gmail.com>
2022-11-03 14:35:15 -0700

    update(): pattern to ensure: at least 1 letter is present

--- src/common/constant/pattern/username.pattern.ts
+++ src/common/constant/pattern/username.pattern.ts
@@ -2,4 +2,4 @@
 /// Matches:
 /// 1) First and Last characters must be alphanumeric
 /// 2) May have hyphen or underscore in the middle
-export const USERNAME_PATTERN = /^[a-zA-Z0-9]+([_-]?[a-zA-Z0-9])*$/g
+export const USERNAME_PATTERN = /^(?=.*[A-Za-z])[A-Za-z\d_-]*/
