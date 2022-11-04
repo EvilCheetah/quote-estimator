99b88066e2f52b71d5921597f2ea00fd3c1e4c19
Eugene Moshchyn <eugene.moschyn@gmail.com>
2022-11-04 14:59:40 -0700

    add(): strategy for jwt validation

---
+++ src/auth/strategy/jwt-access.strategy.ts
@@ -0,0 +1,28 @@
+import { JwtPayload } from "@common/interface";
+import { Injectable } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { PassportStrategy } from "@nestjs/passport";
+import { ExtractJwt, Strategy } from "passport-jwt";
+import { UsersService } from "src/users/users.service";
+
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt-access')
+{
+    constructor(
+        private readonly configService: ConfigService,
+        private readonly usersService:  UsersService 
+    )
+    {
+        super({
+            jwtFromRequest:   ExtractJwt.fromAuthHeaderAsBearerToken(),
+            ignoreExpiration: false,
+            secretOrKey:      configService.get('JWT_ACCESS_TOKEN_SECRET')
+        })
+    }
+
+    validate(validation_payload: JwtPayload)
+    {
+        return this.usersService.findOneBy({ email: validation_payload.email });
+    }
+}
