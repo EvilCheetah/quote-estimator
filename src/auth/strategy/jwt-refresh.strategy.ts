3a97d849b0fd90700bb4d9ee367105c90a9801df
Eugene Moshchyn <eugene.moschyn@gmail.com>
2022-11-07 12:03:35 -0800

    fix(): issue with getting secret from config-service added injectable, which allows to perfor that

--- src/auth/strategy/jwt-refresh.strategy.ts
+++ src/auth/strategy/jwt-refresh.strategy.ts
@@ -2,18 +2,15 @@ import { Request } from "express";
 import { ExtractJwt, Strategy } from "passport-jwt";
 import { ConfigService } from "@nestjs/config";
 import { PassportStrategy } from "@nestjs/passport";
-import { ForbiddenException } from "@nestjs/common";
+import { ForbiddenException, Injectable } from "@nestjs/common";
 
 import { JwtPayload } from "@common/interface";
-import { AuthService } from "../auth.service";
 
 
+@Injectable()
 export class JwtRefreshStrategy extends PassportStrategy(Strategy, 'jwt-refresh')
 {
-    constructor(
-        private readonly configService: ConfigService,
-        private readonly authService:   AuthService
-    )
+    constructor(configService: ConfigService)
     {
         super({
             jwtFromRequest:    ExtractJwt.fromAuthHeaderAsBearerToken(),
