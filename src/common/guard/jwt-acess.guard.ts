0131c1afa7fdde91643961bbe6d7716db081ca6d
Eugene Moshchyn <eugene.moschyn@gmail.com>
2022-11-07 12:17:27 -0800

    update(): jwt-access guard to take into consideration is-public decorator

--- src/common/guard/jwt-acess.guard.ts
+++ src/common/guard/jwt-acess.guard.ts
@@ -1,4 +1,29 @@
+import { Reflector } from "@nestjs/core";
 import { AuthGuard } from "@nestjs/passport";
+import { ExecutionContext, Injectable } from "@nestjs/common";
+
+import { CanActivate } from "@common/types";
+import { IS_PUBLIC_KEY } from "@common/decorator";
 
 
-export class JwtAuthGuard extends AuthGuard('jwt-access') {}
+@Injectable()
+export class JwtAuthGuard extends AuthGuard('jwt-access')
+{
+    constructor(private reflector: Reflector)
+    {
+        super()
+    }
+
+    canActivate(context: ExecutionContext): CanActivate
+    {
+        const isPublic = this.reflector.getAllAndOverride<boolean>(
+            IS_PUBLIC_KEY,
+            [context.getHandler(), context.getClass()]
+        );
+
+        if (isPublic)
+            return true;
+        
+        return super.canActivate(context);
+    }
+}
