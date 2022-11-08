b77885802d24cd898e3f9606ccaed2faead483cf
Eugene Moshchyn <eugene.moschyn@gmail.com>
2022-11-08 14:50:02 -0800

    add(): basic crud

--- src/geolocation/country/country.controller.ts
+++ src/geolocation/country/country.controller.ts
@@ -1,34 +1,59 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe } from '@nestjs/common';
+import { Country } from '@prisma/client';
 import { CountryService } from './country.service';
 import { CreateCountryDTO } from './dto/create-country.dto';
 import { UpdateCountryDTO } from './dto/update-country.dto';
 
+
 @Controller('country')
-export class CountryController {
-  constructor(private readonly countryService: CountryService) {}
+export class CountryController
+{
+    constructor(
+        private readonly countryService: CountryService
+    ) {}
 
-  @Post()
-  create(@Body() createCountryDTO: CreateCountryDTO) {
-    return this.countryService.create(createCountryDTO);
-  }
+    @Post()
+    create(
+        @Body()
+        createCountryDTO: CreateCountryDTO
+    ): Promise<Country>
+    {
+        return this.countryService.create(createCountryDTO);
+    }
 
-  @Get()
-  findAll() {
-    return this.countryService.findAll();
-  }
+    @Get()
+    findAll(): Promise<Country[]>
+    {
+        return this.countryService.findAll();
+    }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.countryService.findOne(+id);
-  }
+    @Get(':country_id')
+    findOne(
+        @Param('country_id', ParseIntPipe)
+        country_id: number
+    ): Promise<Country>
+    {
+        return this.countryService.findOne(country_id);
+    }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateCountryDTO: UpdateCountryDTO) {
-    return this.countryService.update(+id, updateCountryDTO);
-  }
+    @Patch(':country_id')
+    update(
+        @Param('country_id', ParseIntPipe)
+        country_id: number,
+        
+        @Body()
+        updateCountryDTO: UpdateCountryDTO
+    ): Promise<Country>
+    {
+        return this.countryService.update(country_id, updateCountryDTO);
+    }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.countryService.remove(+id);
-  }
+    @Delete(':country_id')
+    remove(
+        @Param('country_id', ParseIntPipe)
+        country_id: number
+    ): Promise<Country>
+    {
+        return this.countryService.remove(country_id);
+    }
 }
--- src/geolocation/country/country.service.ts
+++ src/geolocation/country/country.service.ts
@@ -1,27 +1,57 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Country } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateCountryDTO } from './dto/create-country.dto';
 import { UpdateCountryDTO } from './dto/update-country.dto';
 
 
 @Injectable()
-export class CountryService {
-  create(createCountryDTO: CreateCountryDTO) {
-    return 'This action adds a new country';
-  }
+export class CountryService
+{
+    constructor(
+        private readonly prisma: PrismaService
+    ) {}
 
-  findAll() {
-    return `This action returns all country`;
-  }
+    create(createCountryDTO: CreateCountryDTO): Promise<Country>
+    {
+        return this.prisma.country.create({
+            data: createCountryDTO
+        })
+    }
 
-  findOne(id: number) {
-    return `This action returns a #${id} country`;
-  }
+    findAll(): Promise<Country[]>
+    {
+        return this.prisma.country.findMany();
+    }
 
-  update(id: number, updateCountryDTO: UpdateCountryDTO) {
-    return `This action updates a #${id} country`;
-  }
+    async findOne(country_id: number): Promise<Country>
+    {
+        const country = await this.prisma.country.findUnique({
+            where: { country_id }
+        });
 
-  remove(id: number) {
-    return `This action removes a #${id} country`;
-  }
+        if ( !country )
+            throw new NotFoundException(`Country with id: ${country_id} was NOT FOUND`);
+        
+        return country;
+    }
+
+    async update(country_id: number, updateCountryDTO: UpdateCountryDTO): Promise<Country>
+    {
+        const country = await this.findOne(country_id);
+
+        return this.prisma.country.update({
+            where: { country_id: country.country_id },
+            data:    updateCountryDTO
+        });
+    }
+
+    async remove(country_id: number): Promise<Country>
+    {
+        const country = await this.findOne(country_id);
+
+        return this.prisma.country.delete({
+            where: { country_id: country.country_id }
+        });
+    }
 }
