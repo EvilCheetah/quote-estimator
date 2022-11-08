2e1add9b86a3e0bb4bf84ca4e66af5b04be6c859
Eugene Moshchyn <eugene.moschyn@gmail.com>
2022-11-08 12:51:11 -0800

    convert(): app from fastify to express

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,11 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 
 
 async function bootstrap()
 {
-    const app = await NestFactory.create<NestFastifyApplication>(
-        AppModule,
-        new FastifyAdapter()
-    );
+    const app = await NestFactory.create(AppModule);
 
     app.useGlobalPipes( new ValidationPipe() );
 
