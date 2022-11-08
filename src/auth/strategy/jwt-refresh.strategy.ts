fcbd1c78675e752a5028b5dd0c4daa141a85a03c
Eugene Moshchyn <eugene.moschyn@gmail.com>
2022-11-08 12:57:14 -0800

    add(): logic to valdiate refresh token refresh token is getting compared to the one stored in db

--- src/auth/strategy/jwt-refresh.strategy.ts
+++ src/auth/strategy/jwt-refresh.strategy.ts
@@ -2,30 +2,40 @@ import { Request } from "express";
 import { ExtractJwt, Strategy } from "passport-jwt";
 import { ConfigService } from "@nestjs/config";
 import { PassportStrategy } from "@nestjs/passport";
-import { ForbiddenException, Injectable } from "@nestjs/common";
+import { ForbiddenException, Injectable, Logger, UnauthorizedException } from "@nestjs/common";
 
 import { JwtPayload } from "@common/interface";
+import { AuthService } from "../auth.service";
 
 
 @Injectable()
 export class JwtRefreshStrategy extends PassportStrategy(Strategy, 'jwt-refresh')
 {
-    constructor(configService: ConfigService)
+    constructor(
+        private readonly configService: ConfigService,
+        private readonly authService:   AuthService
+    )
     {
         super({
             jwtFromRequest:    ExtractJwt.fromAuthHeaderAsBearerToken(),
+            passReqToCallback: true,
             ingnoreExpiration: false,
-            secretOrKey:       configService.get('JWT_REFRESH_TOKEN_SECRET')
+            secretOrKey:       configService.get('JWT_REFRESH_TOKEN_SECRET'),
         })
     }
 
     validate(request: Request, validation_payload: JwtPayload)
     {
-        const refresh_token = request?.get('authorization')?.replace('Bearer', '').trim();
+        const refresh_token = request.get('Authorization')?.replace('Bearer', '').trim();
 
         if ( !refresh_token )
             throw new ForbiddenException(`Unable to obtain refresh token`);
         
-        return { ...validation_payload, refresh_token };
+        const user = this.authService.validateRefreshToken(validation_payload.sub, refresh_token);
+
+        if ( !user )
+            throw new UnauthorizedException(`Access denied`);
+
+        return user;
     }
 }
