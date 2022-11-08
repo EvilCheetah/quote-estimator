dd9a1407730448c34011f228a0fae4d1cc983693
Eugene Moshchyn <eugene.moschyn@gmail.com>
2022-11-08 14:57:12 -0800

    initial commit

---
+++ src/geolocation/postal-code/postal-code.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { PostalCodeService } from './postal-code.service';
+import { PostalCodeController } from './postal-code.controller';
+
+@Module({
+  controllers: [PostalCodeController],
+  providers: [PostalCodeService]
+})
+export class PostalCodeModule {}
