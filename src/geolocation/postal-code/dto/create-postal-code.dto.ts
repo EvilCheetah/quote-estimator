44c0cf26c61a651d8e37bdefd3276178fece4109
Eugene Moshchyn <eugene.moschyn@gmail.com>
2022-11-09 13:58:40 -0800

    add(): dto for postal code

--- src/geolocation/postal-code/dto/create-postal-code.dto.ts
+++ src/geolocation/postal-code/dto/create-postal-code.dto.ts
@@ -1,1 +1,18 @@
-export class CreatePostalCodeDTO {}
+import { IsLatitude, IsLongitude, IsPostalCode } from "class-validator";
+import { IsNonNegative } from "@decorator";
+
+
+export class CreatePostalCodeDTO
+{
+    @IsNonNegative()
+    city_id: number;
+
+    @IsPostalCode()
+    postal_code_value: string;
+
+    @IsLatitude()
+    latitude: number;
+
+    @IsLongitude()
+    longitude: number;
+}
