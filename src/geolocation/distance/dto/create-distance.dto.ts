a4ccd8ce7d0c846edb604d45e0c85ba2408df62e
Eugene Moshchyn <eugene.moschyn@gmail.com>
2022-11-10 12:49:02 -0800

    add(): basic structure for distance it is represented as node-edge connection between postal codes

--- src/geolocation/distance/dto/create-distance.dto.ts
+++ src/geolocation/distance/dto/create-distance.dto.ts
@@ -1,1 +1,15 @@
-export class CreateDistanceDTO {}
+import { IsNumber } from "class-validator";
+import { IsID } from "@decorator";
+
+
+export class CreateDistanceDTO
+{
+    @IsID()
+    first_postal_code_node_id:  number;
+
+    @IsID()
+    second_postal_code_node_id: number;
+
+    @IsNumber()
+    ground_distance:            number;
+}
