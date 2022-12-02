f61035055e10685d774c468e622e6845f6d17e83
Eugene Moshchyn <eugene.moschyn@gmail.com>
2022-12-02 13:37:17 -0800

    add: cities for multiple states

---
+++ prisma/data/usa/cities/dc-cities.ts
@@ -0,0 +1,12 @@
+export const DC_CITIES = [
+	["Anacostia",                    "DC"],
+	["Bolling AFB",                  "DC"],
+	["Chevy Chase",                  "DC"],
+	["Fort Lesley J Mcnair",         "DC"],
+	["Fort Mcnair",                  "DC"],
+	["Joint Base Anacostia Bolling", "DC"],
+	["Naval Anacostia Annex",        "DC"],
+	["US Marine Corps Barracks",     "DC"],
+	["Washington",                   "DC"],
+	["Washington Navy Yard",         "DC"]
+]
---
+++ prisma/data/usa/cities/mp_cities.ts
@@ -0,0 +1,5 @@
+export const MP_CITIES = [
+    ["Rota",   "MP"],
+    ["Saipan", "MP"],
+    ["Tinian", "MP"]
+]
