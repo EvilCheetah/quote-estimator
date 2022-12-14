44a2d91beb5e373fb14df5a465be65cddc9b343d
Eugene Moshchyn <eugene.moschyn@gmail.com>
2022-12-14 12:27:10 -0800

    add(): types to country

--- prisma/data/countries.ts
+++ prisma/data/countries.ts
@@ -1,4 +1,8 @@
-export const COUNTRIES = [
+import { CountryName } from "./types/country-name.type";
+import { CountryAbbreviation } from "./types/country-abbreviation.type";
+
+
+export const COUNTRIES: [CountryName, CountryAbbreviation][] = [
     ['United States', 'USA'],
     ['Canada',        'CAN'],
     ['Mexico',        'MEX']
