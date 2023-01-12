4bdb52ee38231eb5dc3fef8993793bf1156a680b
Eugene Moshchyn <eugene.moschyn@gmail.com>
2023-01-12 14:58:43 -0800

    update(): seed with new data

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,10 +1,9 @@
-import { Promise } from "bluebird";
 import { Prisma, PrismaClient } from "@prisma/client";
 
 import { COUNTRIES } from "./data/countries";
 import { US_POSTAL_CODES } from "./data/usa/postal-codes";
-import { US_POSTAL_EDGES } from "./data/usa/us-postal-code.edges";
 import { US_STATES } from "./data/usa/us-states";
+import { VEHICLES } from "./data/vehicles";
 
 
 async function seed(prisma: PrismaClient)
@@ -23,6 +22,18 @@ async function seed(prisma: PrismaClient)
         data:           countries,
         skipDuplicates: true
     })
+    
+    ///--------------- Populate Postal Codes ---------------///
+    await prisma.postalCode.createMany({
+        data: US_POSTAL_CODES.map(
+            ({ postal_code, latitude, longitude }) => ({
+                postal_code_value: postal_code,
+                latitude:          Number(latitude),
+                longitude:         Number(longitude)
+            })
+        ),
+        skipDuplicates: true
+    });
 
     ///--------------- Populate States ---------------///
     const states = US_STATES.map(
@@ -61,75 +72,105 @@ async function seed(prisma: PrismaClient)
     }
 
     ///--------------- Populate Cities ---------------///
-    for (const state of states)
+    for (const { cities, state_abbreviation } of states)
     {
-        const { state_abbreviation, cities } = state;
-
         const { state_id } = await prisma.state.findUnique({
             where: { state_abbreviation }
         });
 
         await prisma.city.createMany({
             data: cities.map(
-                ([ city_name ]) => ({ city_name, state_id })
+                ({ city_name, latitude, longitude, postal_codes }) =>
+                ({
+                    city_name,
+                    latitude:   Number(latitude),
+                    longitude:  Number(longitude),
+                    state_id,
+                })
             ),
             skipDuplicates: true
         });
     }
 
-    ///--------------- Populate Postal Codes ---------------///
-    await prisma.postalCode.createMany({
-        data: Object.keys(US_POSTAL_CODES).map(
-            (postal_code_value) => {
-                const {latitude, longitude} = US_POSTAL_CODES[postal_code_value];
+    ///------------ Connect Cities and Postal Codes -----------///
+    for (const { cities, state_abbreviation } of states)
+    {
+        const existing_postal_codes = await prisma.postalCode.findMany();
+        const cities_in_state       = await prisma.city.findMany({
+            where: { state: { state_abbreviation } }
+        });
 
-                return { postal_code_value, latitude, longitude, city_id: null }
+        const cities_to_postal_codes = []; 
+        
+        for (const { city_name, postal_codes } of cities)
+        {
+            for (const postal_code of postal_codes)
+            {
+                cities_to_postal_codes.push({
+                    city_id:        cities_in_state.find(city => city.city_name === city_name)?.city_id,
+                    postal_code_id: existing_postal_codes.find(code => code.postal_code_value === postal_code)?.postal_code_id
+                })
             }
-        ),
+        }
+
+        await prisma.postalCodesInCities.createMany({
+            data:           cities_to_postal_codes,
+            skipDuplicates: true
+        });
+    }
+
+    ///------------------- Vehicle Types -------------------///
+    const vehicle_types = [
+        ...new Set(
+            VEHICLES.map( (vehicle) => (vehicle.type_name) )
+        )
+    ];
+
+    await prisma.vehicleType.createMany({
+        data: vehicle_types.map( (type) => ({ vehicle_type_name: type }) ),
         skipDuplicates: true
     });
 
-    ///------------ Populate Postal Codes Edges -----------///
-    Promise.map(
-        US_POSTAL_EDGES,
-        async function({ origin, destination, land_distance, water_distance })
-        {
-            const postalPrismaClient = new PrismaClient();
+    ///------------------- Vehicle Makes -------------------///
+    const vehicle_makes = [
+        ...new Set(
+            VEHICLES.map( (vehicle) => (vehicle.make_name) )
+        )
+    ];
 
-            try
-            {
-                await postalPrismaClient.postalEdge.create({
-                    data: {
-                        node:  { connect: { postal_code_value: origin } },
-                        child: { connect: { postal_code_value: destination } },
-                        land_distance,
-                        water_distance
-                    }
-                });
+    await prisma.make.createMany({
+        data: vehicle_makes.map( (make) => ({ make_name: make }) ),
+        skipDuplicates: true
+    });
 
-                await postalPrismaClient.postalEdge.create({
-                    data: {
-                        node:  { connect: { postal_code_value: destination } },
-                        child: { connect: { postal_code_value: origin } },
-                        land_distance,
-                        water_distance
-                    }
-                });
-            }
-            catch (e)
-            {
-                if (e instanceof Prisma.PrismaClientKnownRequestError)
-                {
-                    if (e.code === 'P2002')
-                        console.log(`Leg between "${origin}" and "${destination}" already EXISTS`)
-                }
-            }
+    ///------------------- Vehicle Models -------------------///
+    const types = await prisma.vehicleType.findMany();
+    const makes = await prisma.make.findMany();
 
-            console.log(`Leg between ${origin} and ${destination} has been SUCCESSFULLY created!`)
-            await postalPrismaClient.$disconnect();
-        },
-        { concurrency: 100 }
-    )
+    await prisma.model.createMany({
+        data: VEHICLES.map(
+            ({ year, make_name, model_name, full_model_name, type_name}) => ({
+                year,
+                make_id: makes.find( (o) => (o.make_name === make_name) ).make_id,
+                model_name,
+                full_model_name,
+                vehicle_type_id: types.find( (o) => (o.vehicle_type_name === type_name) ).vehicle_type_id
+            })
+        ),
+        skipDuplicates: true
+    })
+
+    ///------------------- Quote -------------------///
+
+    await prisma.quoteVariable.create({
+        data: {
+            coefficient:       47,
+            power:             0.4565,
+            base_price:        250,
+            inoperational_fee: 150,
+            enclosed_fee:      400
+        }
+    })
 }
 
 
