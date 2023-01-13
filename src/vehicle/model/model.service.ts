9dc721701f9411ca436c4398c69608a60ffb08ea
Eugene Moshchyn <eugene.moschyn@gmail.com>
2023-01-13 09:37:00 -0800

    add(): implementation for years and find_all

--- src/vehicle/model/model.service.ts
+++ src/vehicle/model/model.service.ts
@@ -3,6 +3,7 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { ModelDTO } from './dto/model.dto';
 import { PrismaService } from '@prisma';
 import { ModelAdditionalFields } from '@constant';
+import { ModelQuery } from './dto/model.query';
 
 
 @Injectable()
@@ -19,18 +20,38 @@ export class ModelService
         return this.prisma.model.create({
             data: {
                 ...model_data,
-                dimensions: { create: dimensions }
             },
             include: ModelAdditionalFields
         });
     }
 
-    findAll()
+    async get_all_years()
     {
-        return this.prisma.model.findMany();
+        const vehicles = await this.find_all({} as ModelQuery);
+
+        return [
+            ...new Set(
+                vehicles.map( (vehicle) => (vehicle.year) )
+            )
+        ];
     }
 
-    async findOne(model_id: number)
+    find_all({ year, make_id }: ModelQuery)
+    {
+        return this.prisma.model.findMany({
+            where:  { year, make_id },
+            select: {
+                model_id:        true,
+                make_id:         true,
+                year:            true,
+                model_name:      true,
+                full_model_name: true,
+                vehicle_type:    true
+            }
+        });
+    }
+
+    async find_one(model_id: number)
     {
         const model = await this.prisma.model.findUnique({
             where: { model_id },
@@ -47,7 +68,7 @@ export class ModelService
     {
         const { dimensions, ...model_data } = updateModelDTO;
 
-        const model = await this.findOne(model_id);
+        const model = await this.find_one(model_id);
 
         await this.prisma.dimensions.update({
             where: { model_id: model.model_id },
@@ -65,7 +86,7 @@ export class ModelService
 
     async remove(model_id: number)
     {
-        const model = await this.findOne(model_id);
+        const model = await this.find_one(model_id);
 
         return this.prisma.model.delete({
             where: { model_id: model.model_id }
