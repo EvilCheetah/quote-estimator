a643a02c78a39095aca06fc57f7c568f95f8dbf6
Eugene Moshchyn <eugene.moschyn@gmail.com>
2023-01-13 14:40:02 -0800

    refactor(): to remove errors

--- next.config.js
+++ next.config.js
@@ -1,4 +1,25 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
-  reactStrictMode: true,
+
+nextConfig = {
+    async headers()
+    {
+        return [
+            {
+                source:  '/quote/:path*',
+                headers: [
+                    {
+                        key:   "Access-Control-Allow-Credentials",
+                        value: "true"
+                    },
+                    {
+                        key:   "Access-Control-Allow-Origin",
+                        value: "httos://localhost:3000"
+                    },
+                ],
+            }
+        ];
+    }
 }
+
+
+module.exports = nextConfig
