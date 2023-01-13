e150d628b98a8d8fa89a76acfc7ad02fa6b4744f
Eugene Moshchyn <eugene.moschyn@gmail.com>
2023-01-13 08:51:43 -0800

    remove(): unused files

--- src/geolocation/postal-edges/postal-edges.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PostalEdgesService } from './postal-edges.service';
-import { PostalEdgesController } from './postal-edges.controller';
-
-@Module({
-  controllers: [PostalEdgesController],
-  providers:   [PostalEdgesService]
-})
-export class PostalEdgesModule {}
