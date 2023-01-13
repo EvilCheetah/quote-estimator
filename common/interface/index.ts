7eafd4e5801352c442acc4665051a7e76848c029
Eugene Moshchyn <eugene.moschyn@gmail.com>
2023-01-13 14:53:20 -0800

    add(): for path-mapping

---
+++ common/interface/index.ts
@@ -0,0 +1,11 @@
+export * from './props/quote.props';
+export * from './props/form-wrapper.props';
+export * from './props/forward-button.props';
+export * from './props/previous-button.props';
+export * from './props/text-field-props.interface';
+
+export * from './quote.interface';
+export * from './load-info.interface';
+export * from './coordinates.interface';
+export * from './vehicle-make.interface';
+export * from './vehicle-model.interface';
