74f102936b4bce0d8bd5843ce549d4c9d8e221b7
Eugene Moshchyn <eugene.moschyn@gmail.com>
2023-01-13 09:22:02 -0800

    update(): postal-edges to postal-code-in-city

--- src/geolocation/geolocation.module.ts
+++ src/geolocation/geolocation.module.ts
@@ -3,7 +3,7 @@ import { CityModule } from './city/city.module';
 import { StateModule } from './state/state.module';
 import { CountryModule } from './country/country.module';
 import { PostalCodeModule } from './postal-code/postal-code.module';
-import { PostalEdgesModule } from './postal-edges/postal-edges.module';
+import { PostalCodeInCityModule } from './postal-code-in-city/postal-code-in-city.module';
 
 
 @Module({
@@ -12,7 +12,7 @@ import { PostalEdgesModule } from './postal-edges/postal-edges.module';
         StateModule,
         CityModule,
         PostalCodeModule,
-        PostalEdgesModule
+        PostalCodeInCityModule
     ]
 })
 export class GeolocationModule {}
