9c7c2a924a0c039557a035026f3117ab1c305c1f
Eugene Moshchyn <eugene.moschyn@gmail.com>
2023-01-13 10:22:36 -0800

    add(): transform for class-transformer and cors

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,8 @@ async function bootstrap()
 {
     const app = await NestFactory.create(AppModule);
 
-    app.useGlobalPipes( new ValidationPipe() );
+    app.useGlobalPipes( new ValidationPipe({ transform: true }) );
+    app.enableCors();
 
     await app.listen(3000);
 }
